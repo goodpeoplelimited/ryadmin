0196f88cd430cdf63bf41b7bf5e773e038cdf806
zhaomsdemo <zhaomsdemo@gmail.com>
2024-07-31 22:44:31 -0700

    init commit

---
+++ src/main/java/com/gpl/bes/ryadmin/RyadminApplication.java
@@ -0,0 +1,13 @@
+package com.gpl.bes.ryadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gpl/bes/ryadmin/RyadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpl.bes.ryadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
